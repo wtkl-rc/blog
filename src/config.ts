fec0749a866bfb4e421aacf0abfaa76bee2a77d0
rencai <12621500+wtklrencai@user.noreply.gitee.com>
2026-03-29 17:23:06 +0800

    壁纸

--- src/config.ts
+++ src/config.ts
@@ -437,7 +437,7 @@ export const announcementConfig: AnnouncementConfig = {
 };
 
 export const musicPlayerConfig: MusicPlayerConfig = {
-	enable: true, // 启用音乐播放器功能
+	enable: false, // 启用音乐播放器功能
 	mode: "meting", // 音乐播放器模式，可选 "local" 或 "meting"
 	meting_api:
 		"https://www.bilibili.uno/api?server=:server&type=:type&id=:id&auth=:auth&r=:r", // Meting API 地址
